3356c4226adde42a1991f821e4395a06a92bf31e
Maximiliano Quintana <urumax@gmail.com>
2023-07-20 13:32:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Router from "./components/Router";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Router />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

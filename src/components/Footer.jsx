b25f08f10f841909f5c46a3393d90ddfdafecdc7
Maximiliano Quintana <urumax@gmail.com>
2023-08-03 13:58:37 -0300

    partner footer animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,54 @@
+import { useInView } from "react-intersection-observer";
+import { Col, Container, Row } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react"; // Importa useRef
+
 import "./Footer.css";
 
-import { Col, Container, Row } from "react-bootstrap";
-
 function Footer() {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5, // Puedes ajustar este valor según tus necesidades
+  });
+
+  // Agrega una referencia para el elemento partner-footer
+  const partnerRef = useRef(null);
+  // Agrega una referencia para el texto <p>
+  const textRef = useRef(null);
+  const [textAnimationFinished, setTextAnimationFinished] = useState(false);
+
+  // Usa useEffect para aplicar la animación cuando entre en el viewport
+  useEffect(() => {
+    if (inView) {
+      if (textRef.current) {
+        textRef.current.classList.add("appear-from-left");
+        textRef.current.addEventListener("transitionend", () => {
+          // Marca la animación de texto como finalizada
+          setTextAnimationFinished(true);
+        });
+      }
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    if (textAnimationFinished && partnerRef.current) {
+      partnerRef.current.classList.add("appear-from-right");
+    }
+  }, [textAnimationFinished]);
+
   return (
     <footer id="footer">
-      <Row className="footer-partner">
+      <Row
+        ref={ref}
+        className={`footer-partner  ${inView ? "appear-from-right" : ""}`}
+      >
         <Col xs={12} md={12} lg={4} className="col-footer">
-          <p className="text-partner py-3">Patrocinador de Orejanos</p>
+          <p className="text-partner py-3" ref={textRef}>
+            Nos apoya:
+          </p>
         </Col>
         <Col xs={12} md={12} lg={4} className="col-footer">
           <img
+            ref={partnerRef}
             src="/img/logo_kom.svg"
             alt="OrejanosLogo"
             className="partner-logo"
